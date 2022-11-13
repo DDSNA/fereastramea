488b5f7dc0eb23a01498077496e39c2522e93825
DDSNA <79444147+DDSNA@users.noreply.github.com>
2022-11-13 16:16:20 +0200

    initial commit - proiect facultate

--- src/form1.java
+++ src/form1.java
@@ -1,2 +1,10 @@
+import javax.swing.*;
+
 public class form1 {
+    private JPanel panel1;
+    private JTabbedPane tabbedPane1;
+    private JPanel tab2;
+    private JPanel tab1;
+    private JTextField textField1;
+    private JTextField textField2;
 }
